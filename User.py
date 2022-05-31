ae18796aa2c4b082b1536274b7659c02415240cf
KevinBaoB <kevinbaobui@gmail.com>
2022-05-31 16:42:22 -0500

    First

--- User.py
+++ User.py
@@ -1,10 +1,41 @@
 # your improved User class goes here
 
 class User:
+
     def __init__(self, name, age, nationality):
         self.name = name
         self.age = age
         self.nationality = nationality
+        self.post = []
+    
 
     def __str__(self):
         return f"My name is {self.name}, I'm {self.age} years old and I am {self.nationality}!"
+    
+    @property
+    def get_post(self):
+
+        print('Getting Past Posts: ')
+        return self.post
+    
+    
+    def make_post(self, str):
+        
+        self.str = str
+        self.post.append(str)
+
+    def delete_post(self, index):
+        self.index = index
+        print(f"Deleting post number: {self.index + 1} ")
+        self.post.pop(int(self.index))
+
+
+kevin = User("Kevin", 24, "American")
+
+print(kevin)
+
+kevin.make_post("I made a post")
+kevin.make_post('Another post')
+kevin.delete_post(0)
+
+print(kevin.post)
