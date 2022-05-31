3ae2452de4e90a6b47b07f4051b1a8ef4fdc557c
KevinBaoB <kevinbaobui@gmail.com>
2022-05-31 15:52:55 -0500

    First

--- User.py
+++ User.py
@@ -1,1 +1,10 @@
 # your improved User class goes here
+
+class User:
+    def __init__(self, name, age, nationality):
+        self.name = name
+        self.age = age
+        self.nationality = nationality
+
+    def __str__(self):
+        return f"My name is {self.name}, I'm {self.age} years old and I am {self.nationality}!"
